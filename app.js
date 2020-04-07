55111a0017f1a582b985fed319d7ec3a3447bfb6
Lisa Chandra <liza761c@gmail.com>
2020-04-07 16:57:06 +0530

    testing

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get("/", async function(req, res) {
+app.get("/", async function( , res) {
 	res.render("events.ejs");
 });
 
