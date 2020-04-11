e60cd81fecc99d94ae56b618388f702673492237
Lisa Chandra <52909743+lisa761@users.noreply.github.com>
2020-04-11 18:30:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app.js')
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log("Events page running!");
+	console.log("Events page running! Go to http://localhost:3000/ in your browser");
 });
